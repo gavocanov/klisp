bac75f165f03b1efe4eeba20238d578ddf970a02
Paolo Gavocanov <paolo.gavocanov@infobip.com>
2019-01-26 19:07:38 +0100

    bump version

--- src/commonMain/kotlin/klisp/version.kt
+++ src/commonMain/kotlin/klisp/version.kt
@@ -7,8 +7,8 @@ package klisp;
 const val MAVEN_GROUP : String = "org.cth.klisp"
 const val MAVEN_NAME : String = "klisp"
 const val VERSION : String = "0.0.1"
-const val GIT_REVISION : Int = 41
-const val GIT_SHA : String = "3f509f525383f2ae64ae82ae3ac986eaa7fc1121"
-const val GIT_DATE : String = "2019-01-26T00:00:55Z"
-const val BUILD_DATE : String = "2019-01-26T17:56:52Z"
-const val BUILD_UNIX_TIME : Long = 1548525412945L
+const val GIT_REVISION : Int = 29
+const val GIT_SHA : String = "78058e59975f3528efc0c6f2f8cf759b40f2f646"
+const val GIT_DATE : String = "2019-01-26T18:03:29Z"
+const val BUILD_DATE : String = "2019-01-26T18:04:40Z"
+const val BUILD_UNIX_TIME : Long = 1548525880419L
